bcfc028b0fdf45e9b35d42315c06f1cd870815d9
Adam Debono <adam.debono@nurun.com>
2016-11-24 12:11:03 +1100

    fixed auto layout warning

--- Source/ActivityIndicatorController.swift
+++ Source/ActivityIndicatorController.swift
@@ -26,6 +26,18 @@ public class ActivityIndicatorController: UIAlertController {
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.startAnimating()
         
+        if let subView = self.view.subviews.first {
+            if let constraint = subView.constraints.first(where: {
+                $0.firstItem as? UIView == subView &&
+                $0.secondItem == nil &&
+                $0.firstAttribute == .width &&
+                $0.secondAttribute == .notAnAttribute &&
+                $0.relation == .equal
+            }) {
+                constraint.isActive = false
+            }
+        }
+        
         self.view.addSubview(activityIndicator)
         
         self.view.addConstraint(toItem: activityIndicator, attribute: .centerX, constant: 1)
